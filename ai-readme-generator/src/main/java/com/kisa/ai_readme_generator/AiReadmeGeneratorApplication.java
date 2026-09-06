40ba2ac24586dc80653f7763754968f5622d8212
Kisa <kisazaidi55555@gmail.com>
2026-09-06 21:37:20 +0530

    Initial commit: project setup

---
+++ ai-readme-generator/src/main/java/com/kisa/ai_readme_generator/AiReadmeGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.kisa.ai_readme_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiReadmeGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiReadmeGeneratorApplication.class, args);
+	}
+
+}
---
+++ ai-readme-generator/src/test/java/com/kisa/ai_readme_generator/AiReadmeGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kisa.ai_readme_generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiReadmeGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
